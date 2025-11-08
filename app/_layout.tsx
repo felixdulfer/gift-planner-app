daeabca961136d8e9dd7cd3a80569be087dd8813
Felix Dulfer <felix@dulfer.me>
2025-11-08 22:17:38 +0100

    Remove unused screen routes for events and wishlists from RootLayoutNav

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -25,8 +25,6 @@ function RootLayoutNav() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(auth)" />
       <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="events" />
-      <Stack.Screen name="wishlists" />
     </Stack>
   );
 }
